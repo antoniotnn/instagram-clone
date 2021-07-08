3e7969245ed840d0a8847b168f689d9308240e61
Antonio Pires <tonan@globo.com>
2021-07-08 06:47:23 -0300

    Apresentando  o progresso do upload

--- src/app/home/incluir-publicacao/incluir-publicacao.component.ts
+++ src/app/home/incluir-publicacao/incluir-publicacao.component.ts
@@ -17,6 +17,9 @@ export class IncluirPublicacaoComponent implements OnInit {
   public email: string;
   private imagem: any;
 
+  public progressoPublicacao: string = 'pendente';
+  public porcentagemUpload: number;
+
   public formulario: FormGroup = new FormGroup({
     'titulo': new FormControl(null)
   });
@@ -48,8 +51,12 @@ export class IncluirPublicacaoComponent implements OnInit {
     ).subscribe(() => {
       console.log(this.progresso.status);
       console.log(this.progresso.estado);
+      this.progressoPublicacao = 'andamento'
+
+      this.porcentagemUpload = Math.round(( this.progresso.estado.bytesTransferred / this.progresso.estado.totalBytes ) * 100);
 
       if (this.progresso.status === 'concluido') {
+        this.progressoPublicacao = 'concluido';
         continua.next(false);
       }
     })
