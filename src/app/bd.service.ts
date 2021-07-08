397a4db48e3cd09e674ae9da6ce57ea43c666a8a
Antonio Pires <tonan@globo.com>
2021-07-08 07:35:45 -0300

    Tentando consertar ainda a gravação da imagem e titulo no database e storage

--- src/app/bd.service.ts
+++ src/app/bd.service.ts
@@ -14,26 +14,33 @@ export class Bd {
 
         let nomeImagem = Date.now();
 
-        firebase.storage().ref()
-            .child(`imagens/${nomeImagem}`)
-            .put(publicacao.imagem)
-            .on(firebase.storage.TaskEvent.STATE_CHANGED,
-                //acompanhamento do progresso de upload
-                (snapshot: any) => {
-                    this.progresso.status = 'andamento';
-                    this.progresso.estado = snapshot;
-                    console.log('snapshot capturado no on() ', snapshot);
-                },
-                (error) => {
-                    this.progresso.status = 'erro';
-                    //console.log(error);
-                },
-                () => {
-                   //finalização do processo de upload
-                   this.progresso.status = 'concluido'
-                   //console.log('Upload completo');
-                }
-            )
+        firebase.database().ref(`publicacoes/${btoa(publicacao.email)}`)
+            .push( { titulo: publicacao.titulo } )
+            .then((resposta: any) => {
+                console.log("Key do database: ", resposta);
+            })
+
+
+        // firebase.storage().ref()
+        //     .child(`imagens/${nomeImagem}`)
+        //     .put(publicacao.imagem)
+        //     .on(firebase.storage.TaskEvent.STATE_CHANGED,
+        //         //acompanhamento do progresso de upload
+        //         (snapshot: any) => {
+        //             this.progresso.status = 'andamento';
+        //             this.progresso.estado = snapshot;
+        //             console.log('snapshot capturado no on() ', snapshot);
+        //         },
+        //         (error) => {
+        //             this.progresso.status = 'erro';
+        //             //console.log(error);
+        //         },
+        //         () => {
+        //            //finalização do processo de upload
+        //            this.progresso.status = 'concluido'
+        //            //console.log('Upload completo');
+        //         }
+        //     )
 
         // firebase.database().ref(`publicacoes/${btoa(publicacao.email)}`)
         //     .push( { titulo: publicacao.titulo } );
